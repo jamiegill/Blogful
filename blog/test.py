039cba3b3914470746002f8677fbb247e22edc6d
jamiegill <jamiegill.03@gmail.com>
2017-02-10 02:05:15 +0000

    Extending the blog

---
+++ blog/test.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Text, DateTime
+import datetime
+
+engine = create_engine("postgresql://ubuntu:thinkful@localhost:5432/blogful")
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class Entry(Base):
+    __tablename__ = "entries"
+    id = Column(Integer, primary_key=True)
+    title = Column(String(1024))
+    content = Column(Text)
+    datetime = Column(DateTime, default=datetime.datetime.now)
+    
+Base.metadata.create_all(engine)
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,24 @@
 from flask import render_template, request, redirect, url_for
-
+import datetime
 from . import app
 from .database import session, Entry
 
-PAGINATE_BY = 10
 @app.route("/")
 @app.route("/page/<int:page>")
-def entries(page=1):
+@app.route("/page/<int:page>/limit=<float:paginate_by>")
+@app.route("/page/<int:page>/limit=<int:paginate_by>")
+def entries(page=1, paginate_by=10):
     # Zero-indexed page
+    if isinstance(paginate_by, float):
+        paginate_by=10
     page_index = page - 1
     
     count = session.query(Entry).count()
     
-    start = page_index * PAGINATE_BY
-    end = start + PAGINATE_BY
+    start = page_index * paginate_by
+    end = start + paginate_by
     
-    total_pages = (count - 1) // PAGINATE_BY + 1
+    total_pages = (count - 1) // paginate_by + 1
     has_next = page_index < total_pages - 1
     has_prev = page_index > 0
     
@@ -28,7 +31,15 @@ def entries(page=1):
         has_next=has_next,
         has_prev=has_prev,
         page=page,
-        total_pages=total_pages
+        total_pages=total_pages,
+        paginate_by=paginate_by
+    )
+    
+@app.route("/entry/<int:id>")
+def single_entry(id):
+    entries = session.query(Entry).filter_by(id=id)
+    return render_template("single_entry.html",
+         entries=entries
     )
     
     
@@ -43,5 +54,36 @@ def add_entry_post():
         content=request.form["content"],
     )
     session.add(entry)
+    session.commit()
+    return redirect(url_for("entries"))
+    
+@app.route("/entry/<int:id>/edit", methods=["GET"])
+def edit_entry_get(id):
+    entries = session.query(Entry).filter_by(id=id)
+    return render_template("edit_entry.html",
+    entries = entries
+    )
+    
+@app.route("/entry/<int:id>/edit", methods=["POST"])
+def edit_entry_post(id):
+    title=request.form["title"],
+    content=request.form["content"]
+    update_record = session.query(Entry).filter_by(id=id).first()
+    update_record.title = title
+    update_record.content = content
+    session.commit()
+    return redirect(url_for("edit_entry_post", id=id))
+
+@app.route("/entry/<int:id>/delete", methods=["GET"])
+def delete_entry_get(id):
+    entries = session.query(Entry).filter_by(id=id)
+    return render_template("delete_entry.html",
+    entries = entries
+    )
+
+@app.route("/entry/<int:id>/delete", methods=["POST"])
+def delete_entry_post(id):
+    delete_record = session.query(Entry).filter_by(id=id).first()
+    session.delete(delete_record)
     session.commit()
     return redirect(url_for("entries"))
