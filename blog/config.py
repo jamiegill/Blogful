4d334bd9b34015e9c5af7e964cb65515e1b4d1b0
jamiegill <jamiegill.03@gmail.com>
2017-02-20 01:18:25 +0000

    fixed 'FALSE' bug in config.py

--- blog/config.py
+++ blog/config.py
@@ -11,5 +11,5 @@ class TestingConfig(object):
     
 class TravisConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/blogful-test"
-    DEBUG = FALSE
+    DEBUG = False
     SECRET_KEY = "Not secret"
